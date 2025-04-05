a07e8eee0a5f3734cb0107aa3eb9406922d68515
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2025-04-05 16:47:11 +0700

    hidden-scroll

--- app/(main)/(routes)/my-skills/page.tsx
+++ app/(main)/(routes)/my-skills/page.tsx
@@ -1,51 +1,13 @@
 "use client";
 import React from "react";
-import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/css";
-import Image from "next/image";
-import {Autoplay} from "swiper/modules";
-import {useGetSkillQuery} from "@/redux/feature/skillSlice";
-import {useAppDispatch} from "@/redux/hooks";
-import {SkillData} from "@/constants/constants";
 import GitHubProfile from "@/components/github-profile/GitHubProfile";
 
 const Page = () => {
-  const skills=useGetSkillQuery({});
-  const dispatch=useAppDispatch();
-  const RenderSkillSwiper=({reverseDirection=false}:{reverseDirection:boolean})=>{
-    return  <Swiper
-        slidesPerView={5}
-        loop={true}
-        autoplay={{
-          delay: 0,
-          disableOnInteraction: false,
-          reverseDirection:reverseDirection
-        }}
-        speed={5000}
-        modules={[Autoplay]}
-        className="max-w-[100%] "
-    >
-      {/*skills?.currentData?*/}
-      {SkillData.map((skill, index) => (
-          <SwiperSlide key={skill.name}>
-            <div className={'flex items-center justify-center h-[70px]  w-[70px] lg:h-[100px] lg:w-[100px] border p-[10px]'}
-                 // onClick={()=>dispatch(onOpen({ type: 'modify-skill', data: { skill } }))}
-            >
-              <Image
-                  src={skill.Image}
-                  alt={skill.name}
-                  width={80}
-                  height={80}
-              />
-            </div>
-          </SwiperSlide>
-      ))}
-    </Swiper>
-  }
   return (
     <div
       style={{ backgroundImage: "url(/bg-2.jpg)" }}
-      className="h-screen w-screen flex items-center justify-center bg-cover bg-center"
+      className="h-screen w-screen flex items-center justify-center bg-cover bg-center overflow-hidden"
     >
       <GitHubProfile/>
     </div>
